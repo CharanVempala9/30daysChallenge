ac6da722812890df93c7e836fa5ebda4fd09fc16
CharanVempala9 <119602374+CharanVempala9@users.noreply.github.com>
2025-08-26 19:01:00 +0530

    Time: 1 ms (96%), Space: 44.6 MB (49.23%) - LeetHub

---
+++ 3251-maximum-area-of-longest-diagonal-rectangle/3251-maximum-area-of-longest-diagonal-rectangle.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int areaOfMaxDiagonal(int[][] dimensions) {
+        int n=dimensions.length;
+        double max=0;
+        int ans=0;
+        int finans=0;
+        for(int i=0; i<n; i++){
+            int p1=dimensions[i][0];
+            int p2=dimensions[i][1];
+            double val=Math.sqrt(p1*p1+p2*p2);
+            if(val>max){
+                max=val;
+                ans=p1*p2;
+            }
+            else if(val==max){
+                ans=Math.max(ans,p1*p2);
+            }
+        }
+        return ans;
+    }
+}
