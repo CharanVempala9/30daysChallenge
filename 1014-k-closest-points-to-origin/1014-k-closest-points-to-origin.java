3141eabe447fb271cabcd9d6271c2971918e5837
CharanVempala9 <119602374+CharanVempala9@users.noreply.github.com>
2025-09-06 16:22:26 +0530

    Time: 31 ms (55.63%), Space: 50.8 MB (79.22%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
@@ -1,28 +1,17 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        int[][] res = new int[k][2];
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        HashMap<Integer, List<int[]>> hm = new HashMap<>();
-        
-        for (int[] pt : points) {
-            int x = pt[0];
-            int y = pt[1];
-            int dist = x * x + y * y;  
-            
-            pq.offer(dist);
-            hm.putIfAbsent(dist, new ArrayList<>());
-            hm.get(dist).add(new int[]{x, y});
+        //maxHeap
+        PriorityQueue<int[]>pq=new PriorityQueue<>((a,b)->Integer.compare(b[0]*b[0]+b[1]*b[1],a[0]*a[0]+a[1]*a[1]));
+        for(int[] p:points){
+           pq.offer(p);
+           if(pq.size()>k){
+                pq.poll();
+            } 
         }
-        
-        int i = 0;
-        while (k-- > 0 && !pq.isEmpty()) {
-            int d = pq.poll();
-            int[] point = hm.get(d).remove(0); 
-            res[i][0] = point[0];
-            res[i][1] = point[1];
-            i++;
+        int[][] res=new int[k][2];
+        for(int i=0; i<k; i++){
+            res[i]=pq.poll();
         }
-        
         return res;
     }
 }
