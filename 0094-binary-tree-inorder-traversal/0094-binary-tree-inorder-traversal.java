2a69d785143f162cb170bf5b718eae3d97eb5e20
CharanVempala9 <119602374+CharanVempala9@users.noreply.github.com>
2025-09-11 14:16:40 +0530

    Time: 0 ms (100%), Space: 41.7 MB (45.59%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -1,31 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
+    public void inorder(TreeNode root,List<Integer>ls){
+        if(root==null) return;
+        inorder(root.left,ls);
+        ls.add(root.val);
+        inorder(root.right,ls);
+    }
     public List<Integer> inorderTraversal(TreeNode root) {
-        TreeNode current = root;
-        List<Integer> list = new ArrayList<>();
-
-        while (current != null) {
-            if (current.left == null) {
-                list.add(current.val); // No left child, visit node
-                current = current.right;
-            } else {
-                // Find the inorder predecessor of current
-                TreeNode predecessor = current.left;
-                while (predecessor.right != null && predecessor.right != current) {
-                    predecessor = predecessor.right;
-                }
-
-                if (predecessor.right == null) {
-                    // Establish thread to current
-                    predecessor.right = current;
-                    current = current.left;
-                } else {
-                    // Thread already established, time to visit node
-                    predecessor.right = null;
-                    list.add(current.val); // Visit node after left subtree
-                    current = current.right;
-                }
-            }
-        }
-        return list;
+        List<Integer>ls=new ArrayList<>();
+        inorder(root,ls);
+        return ls;
     }
 }
