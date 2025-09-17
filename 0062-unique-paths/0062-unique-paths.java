9e37cc441395ad0344bb02f5314462fe2c99ac77
CharanVempala9 <119602374+CharanVempala9@users.noreply.github.com>
2025-09-17 23:05:26 +0530

    Time: 0 ms (100%), Space: 40.8 MB (26.29%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int generate(int r,int c,int[][] dp){
+        if(r<0 || c<0){
+            return 0;
+        }
+        if(r==0 && c==0){
+            return 1;
+        }
+        if(dp[r][c]!=-1)return dp[r][c];
+        int up=generate(r-1,c,dp);
+        int left=generate(r,c-1,dp);
+        dp[r][c]= up+left;
+        return dp[r][c];
+    }
+    public int uniquePaths(int m, int n) {
+        int[][] dp=new int[m][n];
+        for(int[] d:dp){
+            Arrays.fill(d,-1);
+        }
+        return generate(m-1,n-1,dp);
+    }
+}
