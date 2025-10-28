8f22c43ecb86a688bb0734ed50cc6eeac26d3bee
CharanVempala9 <119602374+CharanVempala9@users.noreply.github.com>
2025-10-28 10:52:05 +0530

    Time: 4 ms (28.92%), Space: 42.3 MB (59.04%) - LeetHub

--- 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.java
+++ 3616-make-array-elements-equal-to-zero/3616-make-array-elements-equal-to-zero.java
@@ -2,23 +2,20 @@ class Solution {
     public int countValidSelections(int[] nums) {
         int n=nums.length;
         int cnt=0;
+        int[] pf=new int[n];
+        int[] sf=new int[n];
+        pf[0]=nums[0];
+        sf[n-1]=nums[n-1];
+        for(int i=1; i<n; i++)pf[i]=pf[i-1]+nums[i];
+        for(int i=n-2; i>=0; i--)sf[i]=sf[i+1]+nums[i];
         for(int i=0; i<n; i++){
-            if(nums[i]==0){
-                int bsum=0;
-                for(int j=0; j<i; j++){
-                    bsum+=nums[j];
-                }
-                int asum=0;
-                for(int j=i+1; j<n; j++){
-                    asum+=nums[j];
-                }
-                System.out.print(bsum+" "+asum);
-                if(bsum==asum){
-                    cnt+=2;
-                }
-                else if(bsum==asum-1 || asum==bsum-1){
-                    cnt++;
-                }
+            if(nums[i]==0 && pf[i]==sf[i]){
+                cnt+=2;
+                System.out.print("enterd");
+            }
+            else if(nums[i]==0 && (pf[i]==sf[i]-1 || sf[i]==pf[i]-1)){
+                //System.out.println(i);
+                cnt++;
             }
         }
         return cnt;
